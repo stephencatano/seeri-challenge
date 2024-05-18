f5abb0ac2c612cda275b9363f29b51995dba5c86
Stephen Cataño <>
2024-05-18 13:57:28 -0500

    First Commit

---
+++ src/main/java/com/startup/seerichallenge/SeeriChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.startup.seerichallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeeriChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeeriChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/startup/seerichallenge/SeeriChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.startup.seerichallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeeriChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
